7aa2a5ccaafdecdf14d1e8b81994f5504ffc56e6
Shing Hoi Hung <leo@smartretail.co>
2020-06-22 09:52:17 +0800

    Start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn_language_website
+
+# Python 3.7.5
---
+++ speaker.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*
+from gtts import gTTS
+from pygame import mixer
+import tempfile
+import time
+import csv
+
+def speak(sentence, lang):
+    with tempfile.NamedTemporaryFile(delete=True) as fp:
+        tts=gTTS(text=sentence, lang=lang)
+        tts.save('{}.mp3'.format(fp.name))
+        mixer.init()
+        mixer.music.load('{}.mp3'.format(fp.name))
+        mixer.music.play(1)
+    while mixer.music.get_busy():
+        time.sleep(0.1)
+
+def speak_en(sentence):
+    speak(sentence, "en")
+
+def speak_zh(sentence):
+    speak(sentence, "zh-tw")
+
+def save(sentence, lang, filename):
+    with tempfile.NamedTemporaryFile() as fp:
+        tts=gTTS(text=sentence, lang=lang)
+        tts.save((filename+'.mp3').format(fp.name))
+        mixer.init()
+    while mixer.music.get_busy():
+        time.sleep(0.1)
+
+def save_en(sentence, filename):
+    save(sentence, "en", filename)
+    print("File saved to " + filename)
+
+def save_zh_tw(sentence, filename):
+    save(sentence, "zh-tw", filename)
+    print("File saved to " + filename)
+
+def save_ja(sentence, filename):
+    save(sentence, "ja", filename)
+    print("File saved to " + filename)
+
+def save_ko(sentence, filename):
+    save(sentence, "ko", filename)
+    print("File saved to " + filename)
+
+with open('notes.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        save_en(row[0].strip(), "en/" + row[0].strip())
+        save_zh_tw(row[1].strip(), "zh-tw/" + row[0].strip())
+        save_ja(row[2].strip(), "ja/" + row[0].strip())
+        save_ko(row[3].strip(), "ko/" + row[0].strip())
